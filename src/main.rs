12f89cd29468eccdb19892ff16125b973fd1f8fe
James Munns <james.munns@gmail.com>
2016-09-26 14:43:55 +0200

    Add better examples, and better documentations of examples

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,42 @@
 extern crate teensy3;
 
 use teensy3::bindings;
+use teensy3::serial::Serial;
 
 #[no_mangle]
 pub unsafe extern fn main() {
     // Blink Loop
+
     bindings::pinMode(13, bindings::OUTPUT as u8);
+    bindings::digitalWrite(13, bindings::LOW as u8);
+    let mut ser = Serial{};
+
     loop {
+        // Show we are alive
+        alive();
+
+        // If the serial write fails, we will halt (no more alive blinks)
+        hello(&ser).unwrap();
+
+        // Don't spam the console
+        bindings::delay(1000);
+    }
+}
+
+/// Blink the light twice to know we're alive
+pub unsafe fn alive() {
+    for _ in 0..2 {
         bindings::digitalWrite(13, bindings::LOW as u8);
-        bindings::delay(500);
+        bindings::delay(200);
         bindings::digitalWrite(13, bindings::HIGH as u8);
         bindings::delay(200);
+        bindings::digitalWrite(13, bindings::LOW as u8);
+        bindings::delay(200);
     }
+}
+
+/// Send a message over the USB Serial port
+pub fn hello(ser: &Serial) -> Result<(),()> {
+    let msg = "Hello Teensy Rusty World!\n\r";
+    ser.write_bytes(msg.as_bytes())
 }
